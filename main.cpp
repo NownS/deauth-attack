a4f73f8e49059bcf8f5a9f67b8b7b06bdbb77510
NownS <kimjuicy29@gmail.com>
2022-02-04 05:05:16 -0800

    fix typo

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@
 
 void usage() {
     printf("syntax: deauth-attack <interface> <ap mac> [<station mac>] [-auth]\n");
-    printf("sample: deauty-attack mon0 00:11:22:33:44:55 66:77:88:99:AA:BB\n");
+    printf("sample: deauth-attack mon0 00:11:22:33:44:55 66:77:88:99:AA:BB\n");
 }
 
 typedef struct {
